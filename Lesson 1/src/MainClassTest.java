03b6e7dd4008d4bf5e2a1ef013761e4e9a665df2
Eugen-Maximov <81473267+Eugen-Maximov@users.noreply.github.com>
2021-03-28 20:23:31 +0300

    Lesson 1 - Test 2

--- Lesson 1/src/MainClassTest.java
+++ Lesson 1/src/MainClassTest.java
@@ -4,11 +4,18 @@ import org.junit.Test;
 public class MainClassTest {
     MainClass Main = new MainClass();
     int local_number = Main.getLocalNumber();
+    int class_number = Main.getClassNumber();
 
     @Test
    public void testGetLocalNumber()
     {
-        Assert.assertFalse("The 'local_number' variable != 14",local_number != 14);
+        Assert.assertFalse("The 'local_number' variable != 14!",local_number != 14);
+    }
+
+    @Test
+    public void testGetClassNumber()
+    {
+        Assert.assertTrue("The 'class_number' variable <= 45!",class_number > 45);
     }
 
 }
