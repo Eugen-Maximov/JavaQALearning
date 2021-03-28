2adbd3f530de85f0b0c9cc0c03881572ecdbf3d8
Eugen-Maximov <81473267+Eugen-Maximov@users.noreply.github.com>
2021-03-28 19:12:29 +0300

    Test 2 - Lesson 1

--- Lesson 1/src/MainClassTest.java
+++ Lesson 1/src/MainClassTest.java
@@ -1,15 +1,35 @@
 import org.junit.Test;
 
 public class MainClassTest {
-    MainClass Main =new MainClass();
-    int number = Main.getLocalNumber();
+    MainClass Main = new MainClass();
+    int local_number = Main.getLocalNumber();
+    int c_number = Main.getClassNumber();
 
     @Test
-    public void testGetLocalNumber(){
-    if (number == 14){
-        System.out.println("That`s OK");
-    } else {
-        System.out.println("The variable '" + number + "' is not equal to 14 translation");
+   public void testGetLocalNumber(){
+        if (local_number == 14){
+            anExpectedResult("The variable '" + local_number + "' is equal to 14");
+        } else {
+            anErrorMessage("The variable '" + local_number + "' is equal to 14");
+        }
     }
+
+    @Test
+    public void testGetClassNumber(){
+        if (c_number > 45){
+            anExpectedResult("OK");
+        } else if (c_number <= 45){
+            anErrorMessage("Error: " + c_number + " - is not more than 45");
+        }
+    }
+
+    private int anErrorMessage(String error_message){
+        System.out.println(error_message);
+        return 0;
+    }
+
+    private int anExpectedResult(String expected_message){
+        System.out.println(expected_message);
+        return 0;
     }
 }
