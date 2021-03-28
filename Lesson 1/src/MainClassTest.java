be2ab003a3c737770dd3acc7f457b418d809c288
Eugen-Maximov <81473267+Eugen-Maximov@users.noreply.github.com>
2021-03-28 20:59:18 +0300

    Lesson 1 - Test 3

--- Lesson 1/src/MainClassTest.java
+++ Lesson 1/src/MainClassTest.java
@@ -5,6 +5,7 @@ public class MainClassTest {
     MainClass Main = new MainClass();
     int local_number = Main.getLocalNumber();
     int class_number = Main.getClassNumber();
+    String class_string = Main.getClassString();
 
     @Test
    public void testGetLocalNumber()
@@ -18,4 +19,15 @@ public class MainClassTest {
         Assert.assertTrue("The 'class_number' variable <= 45!",class_number > 45);
     }
 
+    @Test
+    public void testGetClassString()
+    {
+        boolean a = true;
+        if (class_string.contains("Hello") | class_string.contains("hello")){
+            a = true;
+        } else {
+            a = false;
+            }
+        Assert.assertTrue("There is no 'Hello' or ''hello' substring in the string",a == true);
+    }
 }
