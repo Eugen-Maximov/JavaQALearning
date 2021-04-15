4df61ce6d038ea5dc05da7a0fba3607b6bfa4e3e
Eugen-Maximov <maximov.evgeny98@gmail.com>
2021-04-15 22:14:36 +0300

    Lesson 4.1
    Swipe

--- JavaAppiumAutomation/src/FirstTest.java
+++ JavaAppiumAutomation/src/FirstTest.java
@@ -1,11 +1,14 @@
+import com.gargoylesoftware.htmlunit.javascript.host.Touch;
 import com.gargoylesoftware.htmlunit.javascript.host.dom.Text;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -134,6 +137,38 @@ public class FirstTest {
                     article_title
             );
         }
+
+    @Test
+    public void testSwipeArticle()
+    {
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find 'Search Wikipedia' input",
+                3
+        );
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                "Java",
+                "Cannot find search input",
+                3
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']"),
+                "Cannot find 'Object-oriented programming language' topic search by 'Java'",
+                5
+        );
+        waitForElementPresent(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "Cannot find article title",
+                10
+        );
+        swipeUp(2000);
+        swipeUp(2000);
+        swipeUp(2000);
+        swipeUp(2000);
+        swipeUp(2000);
+    }
 
         @Test
         public void testForElementHasText()
@@ -269,6 +304,16 @@ public class FirstTest {
             WebDriverWait text = new WebDriverWait(driver, timeOutInSeconds);
             text.withMessage(error_message + "\n");
             return text.until(ExpectedConditions.textToBePresentInElementLocated(by, expected_text));
+        }
+
+        protected void swipeUp(int timeOFSwipe)
+        {
+            TouchAction action = new TouchAction(driver);
+            Dimension size = driver.manage().window().getSize();
+            int x = size.width / 2;
+            int start_y = (int) (size.height * 0.8);
+            int end_y = (int) (size.height * 0.2);
+            action.press(x, start_y).waitAction(timeOFSwipe).moveTo(x, end_y).release().perform();
         }
 }
 
